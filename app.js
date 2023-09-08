f054f98651b8b0c70ad264a9e075e1a6753ade1b
Yeloco22 <yeloco.ramirez@gmail.com>
2023-09-08 13:54:56 -0600

    Se agrega SP de PedidosPendientes_Estado

--- app.js
+++ app.js
@@ -705,9 +705,127 @@ async function getVIS_Calcular_KPI_Produccion_FillRate(params, outs){
 
 }
 
+async function getVIS_Calcular_KPI_PedidosPendientes_Estado(params, outs){
+
+
+
+  var r = await sql.connect(sqlconfig).then(
+    pool => {
+
+
+
+       var RegionZTDem  = params.RegionZTDem === undefined ? null : params.RegionZTDem;
+       var EstadoZTDem  =  params.EstadoZTDem === undefined ? null : params.EstadoZTDem;
+       var ZonaTransporte  =  params. ZonaTransporte === undefined ? null : params. ZonaTransporte;
+       var Cliente =  params.Cliente === undefined ? null : params.Cliente;
+       var Nombre_Cliente  =  params.Nombre_Cliente === undefined ? null : params.Nombre_Cliente;
+       var Obra  =  params.Obra === undefined ? null : params.Obra
+       var Nombre_Obra  =  params.Nombre_Obra === undefined ? null : params.Nombre_Obra;
+       var Frente  =  params.Frente  === undefined ? null : params.Frente ;
+       var Nombre_Frente  =  params.Nombre_Frente === undefined ? null : params.Nombre_Frente;
+       var Segmento  =  params.Segmento === undefined ? null : params.Segmento;
+       var AgrupProducto  =  params.AgrupProducto === undefined ? null : params.AgrupProducto;
+       var Presentacion  =  params.Presentacion === undefined ? null : params.Presentacion;
+       var Producto_Tactician  =  params.Producto_Tactician === undefined ? null : params.Producto_Tactician;
+       var vc50_Region_UN  =  params.vc50_Region_UN === undefined ? null : params.vc50_Region_UN;
+       var GerenciaUN  =  params.GerenciaUN === undefined ? null : params.GerenciaUN;
+       var vc50_UN_Tact  =  params.vc50_UN_Tact === undefined ? null : params.vc50_UN_Tact;
+       var masivos  =  params.masivos === undefined ? null : params.masivos;
+       var idSpider  =  params.idSpider === undefined ? null : params.idSpider;
+
+      // Stored procedure
+
+      var r = pool.request()
+          .input('fechaInicio', params.fechaInicio)
+          .input('fechaFin',params.fechaFin)
+          .input('agrupador',params.agrupador)
+          .input('RegionZTDem' , RegionZTDem)
+          .input('EstadoZTDem', EstadoZTDem)
+          .input('ZonaTransporte', ZonaTransporte)
+          .input('Cliente', Cliente)
+          .input('Nombre_Cliente', Nombre_Cliente)
+          .input('Obra', Obra)
+          .input('Nombre_Obra',Nombre_Obra)
+          .input('Frente', Frente)
+          .input('Nombre_Frente', Nombre_Frente)
+          .input('Segmento', Segmento)
+          .input('AgrupProducto', AgrupProducto)
+          .input('Presentacion',Presentacion)
+          .input('Producto_Tactician', Producto_Tactician)
+          .input('vc50_Region_UN', vc50_Region_UN)
+          .input('GerenciaUN', GerenciaUN )
+          .input('vc50_UN_Tact', vc50_UN_Tact)
+          .input('masivos', masivos)
+          .input('idSpider', idSpider)
+
+
+
+
+          //.output('output_parameter', sql.VarChar(50))
+          .execute('VIS_Calcular_KPI_PedidosPendientes_Estado')
+
+      return (r)
+  }
+  ).then(
+    result => {
+      console.dir(result)
+      return(result)
+
+  }
+  ).catch(
+    err => {
+     console.log(err)
+  }
+
+
+
+  );
+
+
+  return (r)
+
+
+
+}
 
 //ROUTER'S
 
+// Pedidos Pendientes
+
+router.get(['/getSP/VIS_Calcular_KPI_PedidosPendientes_Estado'],(req, res) => {
+
+  let inicio = moment();
+  console.log("Llamada a SP : ********");
+  console.log(req.query);
+
+  res.setHeader('Content-Type', 'application/json');
+
+  getVIS_Calcular_KPI_PedidosPendientes_Estado(req.query,res).then((datos)=>{
+
+            res.setHeader('Content-Type', 'application/json');
+
+            let medio = moment()
+            try{
+             if(datos=== undefined){
+                res.end(JSON.stringify({'error':'timeout'}))
+              } else {
+                res.end(JSON.stringify(datos))
+
+              }
+            } catch {
+              res.end(JSON.stringify({'error':'timeout'}))
+            }
+
+            let fin = moment()
+            console.log("Respondiendo SP en : ", fin.diff(inicio));
+
+    });
+
+
+});
+
+
+//OOS Filial
 router.get(['/getSP/VIS_Calcular_OOSFilial'],(req, res) => {
 
   let inicio = moment();
