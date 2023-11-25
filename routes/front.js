6ec7e11e10cbbbc6a0f679813e557c2eb5789bb1
Ztef <Ztef@hotmail.es>
2023-11-24 21:24:30 -0600

    secured front

--- routes/front.js
+++ routes/front.js
@@ -10,6 +10,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+const isAuthenticated = (req, res, next) => {
+    // Check if the user is authenticated
+    if (req.isAuthenticated()) {
+      return next(); // User is authenticated, proceed to the next middleware
+    } else {
+      // User is not authenticated, handle accordingly (e.g., redirect to login)
+      res.redirect('/auth/signin');
+    }
+  };
+
+
+router.use(isAuthenticated);
 router.use(express.static('front'));
 
 
