1dee3293805e2a46279aba206f4cf5a759204d24
Ztef <Ztef@hotmail.es>
2023-11-24 19:13:58 -0600

    no public

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // Inicio de ruteo :
 app.use('/', indexRouter);
